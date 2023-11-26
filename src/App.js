285f2dffe410e80811deec83491aba57b25bf45a
Tripansh80 <Tripanshgupta5678@gmail.com>
2023-11-26 22:45:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/home/Home";
+import "./styles/app.css"
+import Profile from "./components/profile/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Post from "./components/home/Post";
+import SideBar from "./components/common/SideBar";
+import Login_register from "./components/Login_register";
+import Layout from "./components/Layout";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+    
+      <Routes>
+        
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<SideBar />}></Route>
+
+
+<Route path="/profile" element={<Profile />}></Route>
+<Route path="/login_register" element={<Login_register />}></Route>
+<Route path="/login" element={<Login />}></Route>
+<Route path="/Register" element={<Register />}></Route>
+
+
+      </Routes>
+      </BrowserRouter>
+
+        
     </div>
   );
 }
